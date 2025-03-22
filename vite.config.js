93aea0bd7be6cb326413667d58091dd2d67a80f6
Ali_Shair <alishair7071@gmail.com>
2025-03-23 03:44:06 +0500

    update proxy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api':{
-        target: 'http://192.168.0.108:3000',
+        target: 'https://mern-estate-backend-jet.vercel.app',
         secure: false,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
